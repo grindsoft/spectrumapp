5244bb6a6e61df5a9e7013466ba49e0b2dd6eb7a
Alexandre Ferreira <alexandresgf@gmail.com>
2014-04-04 13:36:34 -0300

    First commit of Spectrum Library

---
+++ app/lib/spectrum.js
@@ -0,0 +1,12 @@
+/**
+ * SPECTRUM LIBRARY
+ *
+ * This is the official library of the Spectrum Portable Console and it is a class that which will be made the
+ * interface between the console hardware and the developer's game.
+ *
+ * @author	Alexandre Ferreira (contato@grindsoft.com.br)
+ */
+
+'use strict';
+
+var slb = {};
